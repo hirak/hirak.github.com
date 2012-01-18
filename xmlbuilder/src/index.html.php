4b92c3d7b83ca57d136a46b43b20fcc3c53456bd
Hiraku NAKANO <hiraku@tojiru.net>
2012-01-19 00:24:58 +0900

    Compress Dojo toolkit

--- xmlbuilder/src/index.html.php
+++ xmlbuilder/src/index.html.php
@@ -4,6 +4,8 @@ require_once 'XML/Builder.php';
 $dt = 'data-dojo-type';
 $dp = 'data-dojo-props';
 $dl = 'dijit.layout';
+$hl_php = array($dt=>'dojox.highlight.Code','class'=>'php');
+$hl_xml = array($dt=>'dojox.highlight.Code','class'=>'xml');
 
 XML_Builder::factory(array('doctype'=>XML_Builder::$HTML5))
 
@@ -12,9 +14,7 @@ XML_Builder::factory(array('doctype'=>XML_Builder::$HTML5))
         ->meta_(array('http-equiv'=>'Content-Type','content'=>'text/html; charset=UTF-8'))
         ->meta_(array('http-equiv'=>'X-UA-Compatible','content'=>'IE=edge,chrome=1'))
         ->title_('php-XML_Builder document')
-        //http://ajax.googleapis.com/ajax/libs/dojo/1.7.1/dijit/themes/claro/claro.css
         ->link_(array('rel'=>'stylesheet','href'=>'http://ajax.googleapis.com/ajax/libs/dojo/1.7.1/dijit/themes/claro/claro.css'))
-        //http://ajax.googleapis.com/ajax/libs/dojo/1.7.1/dojox/highlight/resources/highlight.css
         ->link_(array('rel'=>'stylesheet','href'=>'http://ajax.googleapis.com/ajax/libs/dojo/1.7.1/dojox/highlight/resources/highlight.css'))
         ->link_(array('rel'=>'stylesheet','href'=>'http://fonts.googleapis.com/css?family=Antic'))
         ->style(<<<_CSS_
@@ -22,7 +22,7 @@ body { margin:0;padding:0 }
 #wrapper {
     margin:0 1em;
 }
-h1,h2 {
+h1, h2, h3 {
     font-family: 'Antic', sans-serif;
 }
 h1 {
@@ -64,23 +64,34 @@ _CSS_
                     ->div(array($dt=>"$dl.ContentPane", 'title'=>'Introduction'))
 
                         ->h2_('About')
+                        ->table(array('border'=>1))
+                            ->tr
+                                ->td
+                                    ->pre->code_($hl_php, <<<_PHP_
+<?php
+require_once 'XML/Builder.php';
+XML_Builder::factory()
+
+->root
+    ->hello_('world')
+->_
+
+->_echo;
+_PHP_
+                                    )->_
+                                ->_
+                                ->td
+                                    ->pre->code_($hl_xml, <<<_XML_
+<?xml version="1.0" encoding="UTF-8"?>
+<root>
+  <hello>world</hello>
+</root>
+_XML_
+                                    )->_
+                                ->_
+                            ->_
+                        ->_
                         ->p_('PHPでXMLを組み立てるためのライブラリです。')
-                        ->pre->code_(array($dt=>'dojox.highlight.Code','class'=>'javascript'),<<<_PHP_
-    <?php
-    require_once 'XML/Builder.php';
-    echo XML_Builder::factory()
-    ->root
-        ->hello_('world')
-    ->_;
-_PHP_
-                        )->_
-                        ->pre->code_(array($dt=>'dojox.highlight.Code','class'=>'xml'),<<<_PHP_
-    <?xml version="1.0" encoding="UTF-8"?>
-    <root>
-      <hello>world</hello>
-    </root>
-_PHP_
-                        )->_
                         ->ul
                             ->li
                                 ->strong_('簡潔')
@@ -96,7 +107,7 @@ _PHP_
                             ->_
                             ->li
                                 ->strong_('Arrayサポート')
-                                ->_text(': おまけ機能として、同一インターフェースで配列を組み立てる機能があります。')
+                                ->_text(': おまけ機能として、同一インターフェースで配列を組み立てる機能があります。JSON/XML/PHPSerializeの出し分けが簡単に行えます。')
                             ->_
                         ->_
                         ->hr_
@@ -143,7 +154,7 @@ _TEXT_
                                 ->tr
                                     ->td_('formatOutput')
                                     ->td_('true')
-                                    ->td_('出力を成形するかどうか')
+                                    ->td_('出力を整形するかどうか')
                                 ->_
                                 ->tr
                                     ->td_('version')
@@ -162,84 +173,114 @@ _TEXT_
                                 ->_
                                 ->tr
                                     ->td_('writeto')
-                                    ->td_(':memory:')
+                                    ->td_('memory')
                                     ->td_('XMLWriter専用。XMLWriterが書きだす先を指定します。ファイル名を書けばそのファイルに直接書き込みますし、php://outputを指定すれば標準出力にどんどん書きだしていきます。')
                                 ->_
                             ->_
                         ->_
 
                         ->p_('このXML_Builder::factory()でビルダーオブジェクトを作り、以下のメソッドをメソッドチェーンでつなげて書いていきます。')
-                        ->pre->code_(array($dt=>'dojox.highlight.Code'),<<<_PHP_
+                        ->table(array('border'=>1))
+                            ->tr
+                                ->td
+                                    ->pre->code_($hl_php, <<<_PHP_
 \$builder = XML_Builder::factory(array(
     'class' => 'dom',
     'doctype' => XML_Builder::\$HTML4_STRICT,
 ));
 _PHP_
-                        )->_
+                                    )->_
+                                ->_
+                            ->_
+                        ->_
                         ->hr_
 
                         ->h2_('Methods')
                         ->ul
                             ->li_('引数がない場合は()を省略することができます。')
-                            ->li_('特に理由がなければすべてのpublicメソッドは"xml"を接頭辞に持ちます。xmlから始まる要素はXMLの予約語となっているため、そうそう作ることはないだろうということで決定しました。')
+                            ->li_('「xml」から始まる正式名と、「_」から始まる省略名があります。「xml～」の方がやや高速に動作します。')
                         ->_
                         ->h3_('->xmlElem($name), ->$name')
                         ->p_('$nameという名前の要素を作成して現在編集中の要素に追加します。戻り値は作成した要素になります。')
-                        ->pre->code_(array($dt=>'dojox.highlight.Code'),<<<_PHP_
+                        ->table(array('border'=>1))
+                            ->tr
+                                ->td
+                                    ->pre->code_(array($dt=>'dojox.highlight.Code','class'=>'php'),<<<_PHP_
+//以下はすべて同じ意味
 ->root
-
-/*
+->root()
+->xmlElem('root')
+_PHP_
+                                    )->_
+                                ->_
+                                ->td
+                                    ->pre->code_(array($dt=>'dojox.highlight.Code','class'=>'xml'),<<<_XML_
 <root>
-*/
-_PHP_
-                        )->_
-                        ->p_('名前空間付きの要素や、記号を含む要素の場合は{\'〜\'}で囲う必要があります。xmlElem()なら気にしなくても動きます。')
-                        ->pre->code_(array($dt=>'dojox.highlight.Code'),<<<_PHP_
+_XML_
+                                    )->_
+                                ->_
+                            ->_
+                        ->_
+                        ->p_('名前空間付きの要素や、記号を含む要素の場合は{\'〜\'}で囲う必要があります。')
+                        ->table(array('border'=>1))
+                            ->tr
+                                ->td
+                                    ->pre->code_(array($dt=>'dojox.highlight.Code'),<<<_PHP_
+//以下はすべて同じ意味
 ->{'atom:feed'}
-
-/*
-<atom:feed>
-*/
+->{'atom:feed'}()
+->xmlElem('atom:feed')
 _PHP_
-                        )->_
+                                    )->_
+                                ->_
+                                ->td
+                                    ->pre->code_(array($dt=>'dojox.highlight.Code','class'=>'xml'),<<<_XML_
+<atom:feed>
+_XML_
+                                    )->_
+                                ->_
+                            ->_
+                        ->_
                         ->hr_
 
                         ->h3_('->xmlEnd(), ->_')
                         ->p_('現在の要素の編集を終え、親の要素に戻ります。')
-                        ->pre->code_(array($dt=>'dojox.highlight.Code'),<<<_PHP_
+                        ->pre->code_(array($dt=>'dojox.highlight.Code','class'=>'php'),<<<_PHP_
 ->root
 ->_
-
-/*
-<root/>
-*/
 _PHP_
+                        )->_
+                        ->pre->code_(array($dt=>'dojox.highlight.Code','class'=>'xml'),<<<_XML_
+<root/>
+_XML_
                         )->_
                         ->hr_
 
                         ->h3_('->xmlAttr(), ->_attr()')
                         ->p_('現在編集中の要素に属性を追加します。配列で渡します。')
-                        ->pre->code_(array($dt=>'dojox.highlight.Code'),<<<_PHP_
+                        ->pre->code_(array($dt=>'dojox.highlight.Code','class'=>'php'),<<<_PHP_
 ->root
     ->_attr(array('moge'=>'fuga','noge'=>'guga'))
 ->_
-/*
-<root moge="fuga" noge="guga"/>
-*/
 _PHP_
+                        )->_
+                        ->pre->code_(array($dt=>'dojox.highlight.Code','class'=>'xml'),<<<_XML_
+<root moge="fuga" noge="guga"/>
+_XML_
                         )->_
                         ->hr_
 
                         ->h3_('->xmlText(), ->_text()')
                         ->p_('現在編集中の要素にテキストノードを追加します。')
-                        ->pre->code_(array($dt=>'dojox.highlight.Code'),<<<_PHP_
+                        ->pre->code_(array($dt=>'dojox.highlight.Code','class'=>'php'),<<<_PHP_
 ->root
     ->_text('hogehoge')
 ->_
-/*
-<root>hogehoge</root>
- */
 _PHP_
+                        )->_
+                        ->pre->code_(array($dt=>'dojox.highlight.Code','class'=>'xml'),<<<_XML_
+<root>hogehoge</root>
+_XML_
                         )->_
                         ->hr_
                     ->_
@@ -247,6 +288,15 @@ _PHP_
                     ->div(array($dt=>"$dl.ContentPane", 'title'=>'DOM'))
                         ->h2_('XML_Builder_DOM')
                         ->p_('ここではDOMバックエンド特有の機能を紹介します。')
+                        ->hr_
+
+                        ->h2_('Additional Properties')
+                        ->h3_('->xmlDom')
+                        ->p_('XML_Builderによって構築されたDOMDocumentオブジェクトが格納されています。')
+
+                        ->h2_('Additional Methods')
+                        ->h3_('->xmlEcho($type="xml")')
+                        ->p_('出力を行います。')
                     ->_
 
                     ->div(array($dt=>"$dl.ContentPane", 'title'=>'XMLWriter'))
@@ -275,7 +325,7 @@ _PHP_
                 'dojo.require("dijit.layout.TabContainer");',
                 'dojo.require("dijit.layout.ContentPane");',
                 'dojo.require("dojox.highlight");',
-                'dojo.require("dojox.highlight.languages.javascript");',
+                'dojo.require("dojox.highlight.languages.php");',
                 'dojo.require("dojox.highlight.languages.xml");',
                 '',
             )))
