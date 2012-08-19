a173988fb5d2a89cb8bc1bb554a5eafeaca1d0e6
Hiraku NAKANO <hiraku@tojiru.net>
2012-08-19 11:52:53 +0900

    デザイン修正

--- xmlbuilder/src/index.html.php
+++ xmlbuilder/src/index.html.php
@@ -31,10 +31,10 @@ XML_Builder::factory(array('doctype'=>XML_Builder::$HTML5))
         ->nav(array('class'=>'container'))
           ->h1_(array('class'=>'brand'), 'XML Builder')
           ->ul(array('class'=>'nav'))
-            ->li->a_(array('href'=>'#'),'Introduction')->_
-            ->li->a_(array('href'=>'#'),'Grammer')->_
-            ->li->a_(array('href'=>'#'),'Backends')->_
-            ->li->a_(array('href'=>'#'),'XML ⇔ Array')->_
+            ->li->a_(array('href'=>'#introduction'),'Introduction')->_
+            ->li->a_(array('href'=>'#grammer'),'Grammer')->_
+            ->li->a_(array('href'=>'#backends'),'Backends')->_
+            ->li->a_(array('href'=>'#translate'),'XML ⇔ Array')->_
           ->_
         ->_
       ->_
@@ -45,9 +45,9 @@ XML_Builder::factory(array('doctype'=>XML_Builder::$HTML5))
         ->img_(array('src'=>'assets/xml-builder-logo.png','style'=>'display:block;float:right;margin-top:-25px;'))
         ->h1_('php-XML_Builder')
         ->p_('Most simple DSL for DOM / XMLWriter @ PHP')
-        ->a_(array('class'=>'btn btn-primary btn-large', 'href'=>'#'), 'Download')
+        ->a_(array('class'=>'btn btn-primary btn-large', 'href'=>'https://github.com/hirak/php-XML_Builder'), 'Download')
         ->_text(' ')
-        ->a_(array('class'=>'btn btn-large', 'href'=>'#'), 'Source Code')
+        ->a_(array('class'=>'btn btn-large', 'href'=>'https://github.com/hirak/php-XML_Builder'), 'Source Code')
       ->_
 
       ->section(array('id'=>'introduction'))
@@ -100,158 +100,154 @@ _XML_
             ->_
           ->_
 
-          ->div(array('class'=>'span4'))
-            ->p_(array('style'=>'background-color:black'),'abc')
-          ->_
-
-          ->div(array('class'=>'span4'))
-            ->p_(array('style'=>'background-color:black'),'abc')
-          ->_
-        ->_
-      ->_ //section
-
-      ->div(array('id'=>'wrapper'))
-            ->div
-                ->div
-                    ->div
-
-                        ->hr_
-                        ->h2_('Install')
-                        ->p
-                            ->_text('PEARパッケージ形式で')
-                            ->a_(array('href'=>'http://openpear.org/package/XML_Builder'),'Openpear')
-                            ->_text('に登録してあるため、pearコマンドで簡単にインストールできます。')
-                        ->_
-                        ->pre_(<<<_TEXT_
+          ->div(array('class'=>'span8'))
+            ->h2_('Install')
+            ->h3_('from Openpear')
+            ->p
+              ->_text('PEARパッケージ形式で')
+              ->a_(array('href'=>'http://openpear.org/package/XML_Builder'),'Openpear')
+              ->_text('に登録してあるため、pearコマンドで簡単にインストールできます。')
+            ->_
+            ->pre_(<<<_TEXT_
     # pear channel-discover openpear.org
     # pear install openpear/XML_Builder
 _TEXT_
-                        )
-                        ->p
-                            ->_text('pearコマンドが使えない環境では、')
-                            ->a_(array('href'=>'https://github.com/hirak/php-XML_Builder'), 'github')
-                            ->_text('からソースをダウンロードして、include_pathにlibの中身をコピーすればOK。')
-                        ->_
-                        ->p
-                            ->_text('なお、このページ自体もXML_Builderを使って書いています。ソースは')
-                            ->a_(array('href'=>'https://github.com/hirak/hirak.github.com/blob/master/xmlbuilder/src/index.html.php'),'src/index.html.php')
-                            ->_text('から参照できます。')
-                        ->_
-                    ->_
-                    ->div
-                        ->h2_('XML_Builder::factory()')
-                        ->p_('XML_Builderオブジェクトを生成します。引数は配列で渡します。')
-                        ->table
-                            ->caption_('オプション説明')
-                            ->thead
-                                ->tr
-                                    ->th_('オプション名')
-                                    ->th_('デフォルト')
-                                    ->th_('説明')
-                                ->_
-                            ->_
-                            ->tbody
-                                ->tr
-                                    ->td_('class')
-                                    ->td_('dom')
-                                    ->td_('バックエンドを選択します。dom/xmlwriter/arrayのいずれかを指定します。')
-                                ->_
-                                ->tr
-                                    ->td_('formatOutput')
-                                    ->td_('true')
-                                    ->td_('出力を整形するかどうか')
-                                ->_
-                                ->tr
-                                    ->td_('version')
-                                    ->td_('1.0')
-                                    ->td_('出力するXMLのバージョン。<?xml version="1.0"?>のversionです。')
-                                ->_
-                                ->tr
-                                    ->td_('encoding')
-                                    ->td_('UTF-8')
-                                    ->td_('出力するXMLの文字コード。<?xml version="1.0" encoding="UTF-8"?>のencodingです。')
-                                ->_
-                                ->tr
-                                    ->td_('doctype')
-                                    ->td_('null')
-                                    ->td_('DOCTYPE宣言。array(\'HTML\',null,null)などのように3要素の配列で渡します。よく使いそうなHTML関係のものはXML_Builderクラスのstatic変数として定義してあります。')
-                                ->_
-                                ->tr
-                                    ->td_('writeto')
-                                    ->td_('memory')
-                                    ->td_('XMLWriter専用。XMLWriterが書きだす先を指定します。ファイル名を書けばそのファイルに直接書き込みますし、php://outputを指定すれば標準出力にどんどん書きだしていきます。')
-                                ->_
-                                ->tr
-                                    ->td_('serializer')
-                                    ->td_('var_export')
-                                    ->td_('Array専用。->_echoしたときに適用するcallbackを指定します。ex) json_encode, yaml_emit, XML_Builder::json, serialize')
-                                ->_
-                            ->_
-                        ->_
+            )
+            ->p
+              ->_text('pearコマンドが使えない環境では、')
+              ->a_(array('href'=>'https://github.com/hirak/php-XML_Builder'), 'github')
+              ->_text('からソースをダウンロードして、include_pathにlibの中身をコピーすればOK。')
+            ->_
+            ->p
+              ->_text('なお、このページ自体もXML_Builderを使って書いています。ソースは')
+              ->a_(array('href'=>'https://github.com/hirak/hirak.github.com/blob/master/xmlbuilder/src/index.html.php'),'src/index.html.php')
+              ->_text('から参照できます。')
+            ->_
+          ->_
 
-                        ->p_('このXML_Builder::factory()でビルダーオブジェクトを作り、以下のメソッドをメソッドチェーンでつなげて書いていきます。')
-                        ->table
-                            ->tr
-                                ->td
-                                    ->pre_($pretty, <<<'_PHP_'
+        ->_
+      ->_ //section
+
+      ->section(array('id'=>'grammer'))
+        ->div(array('class'=>'page-header'))
+          ->h1_('Grammer')
+        ->_
+
+        ->div
+          ->h2_('XML_Builder::factory()')
+          ->p_('XML_Builderオブジェクトを生成します。引数は配列で渡します。')
+          ->table(array('class'=>'table table-bordered'))
+            ->caption_('options')
+            ->thead
+              ->tr
+                ->th_('name')
+                ->th_('default value')
+                ->th_('description')
+              ->_
+            ->_
+            ->tbody
+              ->tr
+                ->td_('class')
+                ->td_('dom')
+                ->td_('バックエンドを選択します。dom/xmlwriter/arrayのいずれかを指定します。')
+              ->_
+              ->tr
+                ->td_('formatOutput')
+                ->td_('true')
+                ->td_('出力を整形するかどうか')
+              ->_
+              ->tr
+                ->td_('version')
+                ->td_('1.0')
+                ->td_('出力するXMLのバージョン。<?xml version="1.0"?>のversionです。')
+              ->_
+              ->tr
+                ->td_('encoding')
+                ->td_('UTF-8')
+                ->td_('出力するXMLの文字コード。<?xml version="1.0" encoding="UTF-8"?>のencodingです。')
+              ->_
+              ->tr
+                ->td_('doctype')
+                ->td_('null')
+                ->td_('DOCTYPE宣言。array(\'HTML\',null,null)などのように3要素の配列で渡します。よく使いそうなHTML関係のものはXML_Builderクラスのstatic変数として定義してあります。')
+              ->_
+              ->tr
+                ->td_('writeto')
+                ->td_('memory')
+                ->td_('XMLWriter専用。XMLWriterが書きだす先を指定します。ファイル名を書けばそのファイルに直接書き込みますし、php://outputを指定すれば標準出力にどんどん書きだしていきます。')
+              ->_
+              ->tr
+                ->td_('serializer')
+                ->td_('var_export')
+                ->td_('Array専用。->_echoしたときに適用するcallbackを指定します。ex) json_encode, yaml_emit, XML_Builder::json, serialize')
+              ->_
+            ->_
+          ->_ //table
+
+          ->p_('このXML_Builder::factory()でビルダーオブジェクトを作り、以下のmethodをmethod chainでつなげて書いていきます。')
+          ->pre_($pretty, <<<'_PHP_'
 $builder = XML_Builder::factory(array(
     'class' => 'dom',
     'doctype' => XML_Builder::$HTML4_STRICT,
 ));
 _PHP_
-                                    )
-                                ->_
-                            ->_
-                        ->_
-                        ->hr_
+          )
 
-                        ->h2_('Methods')
-                        ->ul
-                            ->li_('引数がない場合は()を省略することができます。')
-                            ->li_('「xml」から始まる正式名と、「_」から始まる省略名があります。「xml～」の方がやや高速に動作します。')
-                        ->_
-                        ->h3_('->xmlElem($name), ->$name')
-                        ->p_('$nameという名前の要素を作成して現在編集中の要素に追加します。戻り値は作成した要素になります。')
-                        ->table
-                            ->tr
-                                ->td
-                                    ->pre_($pretty,<<<'_PHP_'
+          ->h2_('Methods')
+          ->ul
+            ->li_('引数がない場合は()を省略することができます。')
+            ->li_('「xml」から始まる正式名と、「_」から始まる省略名があります。「xml～」の方がやや高速に動作します。')
+          ->_
+
+          ->h3_('->xmlElem($name), ->$name')
+          ->p_('$nameという名前の要素を作成して現在編集中の要素に追加します。戻り値は作成した要素になります。')
+          ->table(array('class'=>'table'))
+            ->tr
+              ->td
+                ->pre_($pretty,<<<'_PHP_'
 //以下はすべて同じ意味
 ->root
 ->root()
 ->xmlElem('root')
 _PHP_
-                                    )
-                                ->_
-                                ->td
-                                    ->pre_($pretty,<<<'_XML_'
+                )
+              ->_
+              ->td
+                ->pre_($pretty,<<<'_XML_'
 <root>
 _XML_
-                                    )
-                                ->_
-                            ->_
-                        ->_
-                        ->p_('名前空間付きの要素や、記号を含む要素の場合は{\'〜\'}で囲う必要があります。')
-                        ->table
-                            ->tr
-                                ->td
-                                    ->pre_($pretty,<<<'_PHP_'
+                )
+              ->_
+            ->_
+          ->_ //table
+
+          ->p_('名前空間付きの要素や、記号を含む要素の場合は{\'〜\'}で囲う必要があります。')
+          ->table(array('class'=>'table'))
+            ->tr
+              ->td
+                ->pre_($pretty,<<<'_PHP_'
 //以下はすべて同じ意味
 ->{'atom:feed'}
 ->{'atom:feed'}()
 ->xmlElem('atom:feed')
 _PHP_
-                                    )
-                                ->_
-                                ->td
-                                    ->pre_($pretty,<<<'_XML_'
+                )
+              ->_
+              ->td
+                ->pre_($pretty,<<<'_XML_'
 <atom:feed>
 _XML_
-                                    )
-                                ->_
-                            ->_
-                        ->_
-                        ->hr_
+                )
+              ->_
+            ->_
+          ->_//table
+        ->_
+      ->_ //section
+      ->div(array('id'=>'wrapper'))
+            ->div
+                ->div
+                    ->div
+
 
                         ->h3_('->xmlEnd(), ->_')
                         ->p_('現在の要素の編集を終え、親の要素に戻ります。')
